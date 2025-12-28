e8e08633510a99021863e5d8981d79249e9ef438
TouqeerChamp <touqeer@example.com>
2025-12-29 00:38:30 +0500

    Fix build error: Remove non-existent TechnicalInsights export

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,4 +1,3 @@
 export { default as Hero } from './Hero'
 export { default as PortfolioGrid } from './PortfolioGrid'
 export { default as ProjectDiscovery } from './ProjectDiscovery'
-export { default as TechnicalInsights } from './TechnicalInsights'
